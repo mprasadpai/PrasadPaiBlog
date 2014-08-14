94c3435d971511038c97298acdfd182d8f39eb83
mprasadpai <mprasadpai@gmail.com>
2014-08-14 20:14:43 +0530

    FeedBack & Tracker added

--- Home.php
+++ Home.php
@@ -1,15 +1,71 @@
 <html>
-<head><Title>Prasad Pai Blog</Title></head>
+<head><Title>Prasad Pai Blog</Title>
+<link rel="stylesheet" type="text/css" href="Feedback/Feedback.css">
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+<script type="text/javascript" src="Feedback/FeedBack.js"></script>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+</head>
 <body>
-<?php
+
+<h1>Prasad Pai Blog</h1>
+
+<a href="#visitorsForm"><input type="button" id="btnVisitors" value="Visitors"></input></a>
+<a href="#contactForm"><input type="button" id="btnFeedback" value="Feedback"></input></a>
+
+<form id="visitorsForm" method="post">
+
+ <h2>Visitors Details</h2>
+  <div id="formButtons">    
+  
+  <?php
 include ( $_SERVER['DOCUMENT_ROOT'] . '/PrasadPaiBlog/User_Tracker/visitor_tracking.php');
 include ( $_SERVER['DOCUMENT_ROOT'] . '/PrasadPaiBlog/User_Tracker/display_visits.php');
-
-echo 'Welcome to Prasad Pai Blog';
-echo '<br/><br/>';
-
-echo '</br> You are Visitor Number :' . $result->num_rows .'</br>';
-
+echo 'You are Visitor Number :' . $result->num_rows .'</br>';
+showVisitors($visitors);
 ?>
+  
+  <input type="button" id="cancel1" name="cancel" value="Cancel" />
+  </div>
+
+</form>
+
+<form id="contactForm" action="processForm.php" method="post">
+
+  <h2>Send me a mail</h2>
+
+  <ul>
+
+    <li>
+      <label for="senderName">Your Names</label>
+      <input type="text" name="senderName" id="senderName" placeholder="Please type your name" required="required" maxlength="40" />
+    </li>
+
+    <li>
+      <label for="senderEmail">Your Email Address</label>
+      <input type="email" name="senderEmail" id="senderEmail" placeholder="Please type your email address" required="required" maxlength="50" />
+    </li>
+
+    <li>
+      <label for="message" style="padding-top: .5em;">Your Message</label>
+      <textarea name="message" id="message" placeholder="Please type your message" required="required" cols="80" rows="10" maxlength="10000"></textarea>
+    </li>
+
+  </ul>
+
+  <div id="formButtons">
+    <input type="submit" id="sendMessage" name="sendMessage" value="Send Email" />
+    <input type="button" id="cancel" name="cancel" value="Cancel" />
+  </div>
+
+</form>
+
+<div id="sendingMessage" class="statusMessage"><p>Sending your message. Please wait...</p></div>
+<div id="successMessage" class="statusMessage"><p>Thanks for sending your message! We'll get back to you shortly.</p></div>
+<div id="failureMessage" class="statusMessage"><p>There was a problem sending your message. Please try again.</p></div>
+<div id="incompleteMessage" class="statusMessage"><p>Please complete all the fields in the form before sending.</p></div>
+
+
+
+
 </body>
 </html>
--- User_Tracker/display_visits.php
+++ User_Tracker/display_visits.php
@@ -1,9 +1,7 @@
 <?php 
 require_once('visitors_connections.php');//the file with connection code and functions
 
-/* if ($_GET['start'] == "") $start = 0;
-else $start = $_GET['start'];
-$limit = 15;  */
+
 
 $result = mysqli_query($visitors,"SELECT * FROM visitors_table");
 
@@ -11,29 +9,27 @@ $result = mysqli_query($visitors,"SELECT * FROM visitors_table");
 function showVisitors($visitors)
 {
 
-
+$result = mysqli_query($visitors,"SELECT TOP 7 * FROM visitors_table LIMIT 7");
 echo '<table style="width:100%; border:1px dashed #CCC" cellpadding="3">
 <tr>
 <td style="width:15%;border-bottom:1px solid #CCC">IP</td>
-<td style="width:15%;border-bottom:1px solid #CCC">Browser</td>
+<td style="width:15%;border-bottom:1px solid #CCC">Date</td>
 <td style="width:15%;border-bottom:1px solid #CCC">Time</td>
-<td style="width:30%;border-bottom:1px solid #CCC">Refferer</td>
-<td style="width:25%;border-bottom:1px solid #CCC">Page</td>
+
 </tr>';
 
 
 while($row_visitors = mysqli_fetch_array($result)) {
  echo '<tr>
         <td>'.$row_visitors['visitors_ip'].'</td>
-        <td>'.$row_visitors['visitors_browser'].'</td>
+        <td>'.$row_visitors['visitors_day'].'-'.$row_visitors['visitors_month'].'-'.$row_visitors['visitors_year'].'</td>
         <td>'.$row_visitors['visitors_hour'].':'.$row_visitors['visitors_minute'].'</td>
-        <td>'.$row_visitors['visitors_refferer'].'</td>
-        <td>'.$row_visitors['visitors_page'].'</td>
        </tr>';
 }
 mysqli_close($visitors);
 
 echo'</table>';
+
 }
 
 
